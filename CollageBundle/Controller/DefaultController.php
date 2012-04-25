7eff490da9369db8c17aa08b4871fd8dc2fc065a
Ryan McGowan <ryan@ryanmcg.com>
2012-04-25 15:17:38 -0400

    Modified default route and controller.

--- CollageBundle/Controller/DefaultController.php
+++ CollageBundle/Controller/DefaultController.php
@@ -8,8 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
-    {
-        return $this->render('OSUCollageBundle:Default:index.html.twig', array('name' => $name));
+    public function indexAction() {
+        return $this->render('OSUCollageBundle:Default:index.html.twig');
     }
 }
