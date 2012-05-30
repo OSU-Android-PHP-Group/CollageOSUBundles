61a8843390abd871e2c261c77bf2ac6cc7bbe39f
Mark Nguyen <pnguyen1097@gmail.com>
2012-05-30 09:02:29 -0400

    Add some functions to ImageController

--- CollageAPIBundle/Controller/ImageController.php
+++ CollageAPIBundle/Controller/ImageController.php
@@ -21,4 +21,31 @@ class ImageController extends Controller
     public function postImageAction() {
         return array("success" => true);
     }
+
+    //Pass $arr in as $_FILES['images']
+    private function getImageArray($arr) {
+        $result = [];
+        foreach ($arr as $imageFile) {
+            $fileName = $imageFile['tmp_name'];
+            if ($imageFile['type']='image/jpeg') {
+                $img = imagecreatefromjpeg($imageFile['tmp_name']);
+            } else if ($imageFile['type'] = 'image/png') {
+                $img = imagecreatefrompng($imageFile['tmp_name']);
+            } else {
+                $img = imagecreatefromgif($imageFile['tmp_name']);
+            }
+            $result[] = $img;
+        }
+        return $result;
+    }
+
+    private function resizeImage($img, $factor = 0.5) {
+        $width = imagesx($img);
+        $height = imagesy($img);
+        $result = imagecreatetruecolor( $width /2 , $height/2);
+        imagecopyresized($result, $img, 0, 0, 0, 0, $width * $factor, $height * $factor, $width, $height);
+
+        return $result;
+    }
+
 }
